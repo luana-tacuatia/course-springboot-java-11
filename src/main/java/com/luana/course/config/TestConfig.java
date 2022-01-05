1f0e1cfa7c0b927697bebfcc28c963a7cbab0f80
luana-tacuatia <tacuatia@yahoo.com.br>
2022-01-05 12:05:32 -0300

    feat: Category

--- src/main/java/com/luana/course/config/TestConfig.java
+++ src/main/java/com/luana/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.luana.course.config;
 
+import com.luana.course.entities.Category;
 import com.luana.course.entities.Order;
 import com.luana.course.entities.User;
 import com.luana.course.entities.enums.OrderStatus;
+import com.luana.course.repositories.CategoryRepository;
 import com.luana.course.repositories.OrderRepository;
 import com.luana.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
@@ -33,7 +44,7 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
-        userRepository.saveAll(Arrays.asList(u1,u2));
+        userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
     }
